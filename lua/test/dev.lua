adc1b0b1c8d244d50c92543c3f1ebdcb7649da00
Matteo Bouvier <m.bouvier@vidium-solutions.com>
2024-02-04 19:30:12 +0100

    reload command (for dev)

--- lua/test/dev.lua
+++ lua/test/dev.lua
@@ -1,6 +1,6 @@
 vim.opt.rtp:append('/home/mbouvier/git/blame-multi.nvim')
 
-local function reload()
+vim.api.nvim_create_user_command("DevReload", function(opts)
     package.loaded['blame-multi'] = nil
     package.loaded['blame-multi.blame'] = nil
     package.loaded['blame-multi.buffer'] = nil
@@ -8,19 +8,12 @@ local function reload()
     package.loaded['blame-multi.utils'] = nil
 
     require('blame-multi').setup()
-end
-
-vim.api.nvim_create_user_command("DevBlameToggle", function(opts)
-    reload()
-    require('blame-multi').toggle_blame()
 end, {})
 
 vim.api.nvim_create_user_command("DevLogClear", function(opts)
-    reload()
     require('blame-multi.logger'):clear()
 end, {})
 
 vim.api.nvim_create_user_command("DevLog", function(opts)
-    reload()
     require('blame-multi.logger'):show()
 end, {})
